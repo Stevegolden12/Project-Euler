5b6a582238ae45c2b3cacfeb2ab3cb4d9fb67617
Steve Golden <sgolden2525@outlook.com>
2020-01-26 00:00:41 -0600

    Here we go
    asdf

--- problem3.js
+++ problem3.js
@@ -4,8 +4,8 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the given number ? **/
 
 function largestPrimeFactor(number) {
-  let arrayPrimeNumbers = [];
-  asdf
+  let arrayPrimeNumbers = [1,2];
+
   // Good luck!
   return true;
 }
